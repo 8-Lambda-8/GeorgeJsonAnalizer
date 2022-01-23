d2a5e4f6291fae217c49f394f2946ebef6ed5d22
8-Lambda-8 <j.wasle111@gmail.com>
2022-01-23 12:49:44 +0100

    added some stats to stat component

--- src/app/stat/stat.component.ts
+++ src/app/stat/stat.component.ts
@@ -12,11 +12,41 @@ export class StatComponent implements OnInit {
 
   n: Transaction[];
 
-  constructor(private transactionService: TransactionService) { 
+  constructor(private transactionService: TransactionService) {
     this.n = this.transactionService.transactions
   }
 
   ngOnInit(): void {
+  }
+
+  numUncategorized(): number {
+    return this.n.filter(t => t.categories == null).length;
+  }
+
+  deltaAll(): string {
+    let delta = 0;
+    for (let t of this.n) {
+      delta += t.amount.valueFloat
+    }
+    return delta.toLocaleString('de-DE', { style: 'currency', currency: "EUR" });
+  }
+
+  inAll(): string {
+    let _in = 0;
+    for (let t of this.n) {
+      if (t.amount.value>0) 
+      _in += t.amount.valueFloat
+    }
+    return _in.toLocaleString('de-DE', { style: 'currency', currency: "EUR" });
+  }
+
+  outAll(): string {
+    let out = 0;
+    for (let t of this.n) {
+      if (t.amount.value<0) 
+        out += t.amount.valueFloat
+    }
+    return out.toLocaleString('de-DE', { style: 'currency', currency: "EUR" });
   }
 
 }
